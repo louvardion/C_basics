11acd596d69249ac341f4650f16a3fc55fac1dcb
louvardion <diegoamaral321@gmail.com>
2025-02-12 19:26:01 -0300

    2nd Commit - First exercises

---
+++ Exercises/Hello World!/Aula 1_1 HelloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //será incluído na maioria dos programas, pois facilita a interação do usuário com o programa.
+#include <stdlib.h> //biblioteca que contém o "system("pause");"
+int main(){
+	
+	printf("Hello, world!\n");
+	
+	system("pause");
+	
+}
